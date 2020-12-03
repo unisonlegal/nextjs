3e4e47a90578a8b5ba39a13bf5e5c7551a46acc6
Sandeep Tewari <sandeep@unisonlegal.in>
2020-12-03 11:05:06 +0000

    LP-1 is it ok

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
 import Head from 'next/head'
+
 import HeroSection from '../components/HeroSection'
+import Navbar from '../components/NavBar'
 
 
 export default function Home() {
   return (
 
   
+<>
+      <Head>
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css"
+      />
 
-      <HeroSection />
-
-
-    
-
-    
-    // <div>
-    //   <h1>Kozentic Designs</h1>
+      </Head>
+      {/* <Navbar /> */}
       
-
-    // </div>
-    
+      <HeroSection />
+  </>  
   )
 }
